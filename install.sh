6b96ab8a3093178505db981ee4bd513f1c699953
Bashar Nozibulla <nbashar01@gmail.com>
2022-01-31 13:51:30 +0600

    install procedure added

---
+++ install.sh
@@ -0,0 +1,48 @@
+                                         #!/bin/sh
+
+FILE=.env
+if [ ! -f "$FILE" ]; then
+    echo 'Creating .env file...'
+    cp .env.example .env
+fi
+
+echo "Installing composer dependencies..."
+docker run --rm \   
+    -u "$(id -u):$(id -g)" \
+    -v $(pwd):/var/www/html \
+    -w /var/www/html \
+    laravelsail/php74-composer:latest \
+    composer install --ignore-platform-reqs
+
+echo "Stopping running sail container if any..."
+./vendor/bin/sail down
+
+echo "Starting app container..."
+./vendor/bin/sail up -d
+
+echo "Generating application key..."
+./vendor/bin/sail artisan key:generate
+
+sleep 30 & PID=$! #simulate a long process
+
+echo "Processing please be patient..."
+printf "["
+# While process is running...
+while kill -0 $PID 2> /dev/null; do
+    printf  "▇"
+    sleep 1
+done
+echo  "]"
+
+echo "Migrating Tables..."
+./vendor/bin/sail artisan migrate
+
+FILE=public/storage
+if [ ! -h "$FILE" ]; then
+    echo "Linking storage path..."
+    ./vendor/bin/sail artisan storage:link
+fi
+
+echo "Installation successfull!"
+
+echo "- Goto : http://localhost/"
---
+++ redame.md
@@ -0,0 +1,21 @@
+# Laravel with NextJs
+
+Taylor's working version of `laravel-breeze` with `nextjs`
+
+## Installation
+
+### Installing API
+
+```bash
+bash install.sh
+```
+
+### Installing Frontend
+
+```bash
+cd frontend
+
+yarn
+
+yarn dev
+```
